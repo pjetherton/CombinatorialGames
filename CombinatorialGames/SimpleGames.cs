fca74724959d22e5f5b5c0248fb46b0da8303a5f
PJE <paul@pjetherton.co.uk>
2014-12-05 18:22:04 +0000

    Introduced the ChoicelessGame class.

--- CombinatorialGames/SimpleGames.cs
+++ CombinatorialGames/SimpleGames.cs
@@ -6,6 +6,34 @@ using System.Linq;
 
 namespace CombinatorialGames
 {
+    public class ChoicelessGame : CombinatorialGame
+    {
+        public ChoicelessGame(
+            ChoicelessGame leftGame,
+            ChoicelessGame rightGame)
+        {
+            _LeftGame = leftGame;
+            _RightGame = rightGame;
+        }
+
+        public override IEnumerable<CombinatorialGame> Children(Player player)
+        {
+            var game = (player == Player.Left ? _LeftGame : _RightGame);
+            if (game != null)
+            {
+                yield return game;
+            }
+            else
+            {
+                yield break;
+            }
+        }
+
+        protected ChoicelessGame _LeftGame;
+
+        protected ChoicelessGame _RightGame;
+    }
+
     public class IntegerGame : CombinatorialGame
     {
         public IntegerGame(int integer)
