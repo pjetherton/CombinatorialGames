1f5052e70c950511973ddc759000605da31a32e9
PJE <paul@pjetherton.co.uk>
2014-12-05 18:27:49 +0000

    Very elegant refactor of IntegerGame as a ChoicelessGame.

--- CombinatorialGames/SimpleGames.cs
+++ CombinatorialGames/SimpleGames.cs
@@ -34,29 +34,13 @@ namespace CombinatorialGames
         protected ChoicelessGame _RightGame;
     }
 
-    public class IntegerGame : CombinatorialGame
+    public class IntegerGame : ChoicelessGame
     {
         public IntegerGame(int integer)
+            : base(
+                leftGame: (integer > 0 ? new IntegerGame(integer - 1) : null),
+                rightGame: (integer < 0 ? new IntegerGame(integer + 1) : null))
         {
-            this._Integer = integer;
         }
-
-        public override IEnumerable<CombinatorialGame> Children(Player player)
-        {
-            if (player == Player.Left && _Integer > 0)
-            {
-                yield return new IntegerGame(_Integer - 1);
-            }
-            else if (player == Player.Right && _Integer < 0)
-            {
-                yield return new IntegerGame(_Integer + 1);
-            }
-            else
-            {
-                yield break;
-            }
-        }
-
-        private int _Integer;
     }
 }
