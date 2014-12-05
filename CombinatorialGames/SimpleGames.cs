71cb28f9371fb85b1881c76e14b3ba17ea818e61
PJE <paul@pjetherton.co.uk>
2014-12-05 18:02:29 +0000

    Added IntegerGame.

---
+++ CombinatorialGames/SimpleGames.cs
@@ -0,0 +1,34 @@
+﻿/* Copyright (c) 2014 Paul Etherton
+ */
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CombinatorialGames
+{
+    public class IntegerGame : CombinatorialGame
+    {
+        public IntegerGame(int integer)
+        {
+            this._Integer = integer;
+        }
+
+        public override IEnumerable<CombinatorialGame> Children(Player player)
+        {
+            if (player == Player.Left && _Integer > 0)
+            {
+                yield return new IntegerGame(_Integer - 1);
+            }
+            else if (player == Player.Right && _Integer < 0)
+            {
+                yield return new IntegerGame(_Integer + 1);
+            }
+            else
+            {
+                yield break;
+            }
+        }
+
+        private int _Integer;
+    }
+}
