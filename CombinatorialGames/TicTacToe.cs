0690855b3bba0dc08f6e13279b522b983d0571bc
PJE <paul@pjetherton.co.uk>
2014-12-08 06:56:21 +0000

    Removed unused usings.

--- CombinatorialGames/TicTacToe.cs
+++ CombinatorialGames/TicTacToe.cs
@@ -5,8 +5,6 @@ namespace CombinatorialGames
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Linq.Expressions;
 
     public class TicTacToe : CombinatorialGame
     {
